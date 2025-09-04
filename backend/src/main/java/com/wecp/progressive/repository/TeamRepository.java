fb3ff3660f6c865de8db58c90eafc7b4a7aa494d
Swetha Priya <140045041+Swetha-Priya-Desireddy@users.noreply.github.com>
2025-09-04 09:42:45 +0530

    Day 7 Task

--- backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.wecp.progressive.entity.Team;
+
 @Repository
-public interface TeamRepository extends JpaRepository<Team,Integer>{
+public interface TeamRepository extends JpaRepository<Team, Integer>{
     Team findByTeamId(int teamId);
-    
+
 }
