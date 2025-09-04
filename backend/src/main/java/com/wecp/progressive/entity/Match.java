b5705071ededc5d41e4326e3e628d4e130b03658
Swetha Priya <140045041+Swetha-Priya-Desireddy@users.noreply.github.com>
2025-09-04 09:24:18 +0530

    Day 7 Task

--- backend/src/main/java/com/wecp/progressive/entity/Match.java
+++ backend/src/main/java/com/wecp/progressive/entity/Match.java
@@ -2,98 +2,95 @@ package com.wecp.progressive.entity;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
-@Entity(name = "matches")
+@Entity
+@Table(name = "matches")
 public class Match {
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private int matchId;
- @ManyToOne(cascade = CascadeType.MERGE)
- @JoinColumn(name="first_team_id")
- private Team firstTeam;
- @ManyToOne(cascade = CascadeType.MERGE)
- @JoinColumn(name="second_team_id")
- private Team secondTeam;
- @Temporal(TemporalType.DATE)
- private Date matchDate;
- private String venue;
- private String result;
- private String status;
- @ManyToOne
- @JoinColumn(name="winner_team_id")
- private Team winnerTeam;
-public Match() {
-}
-public Match(int matchId, int firstTeamId, int secondTeamId, Date matchDate, String venue, String result, String status,
-        int winnerTeam) {
-    this.matchId = matchId;
-    this.firstTeam.setTeamId(matchId); 
-    this.secondTeam.setTeamId(matchId); 
-    this.matchDate = matchDate;
-    this.venue = venue;
-    this.result = result;
-    this.status = status;
-    this.winnerTeam.setTeamId(matchId);
 
-}
-public int getMatchId() {
-    return matchId;
-}
-public void setMatchId(int matchId) {
-    this.matchId = matchId;
-}
-public Team getFirstTeam() {
-    return firstTeam;
-}
-public void setFirstTeam(Team firstTeam) {
-    this.firstTeam = firstTeam;
-}
-public Team getSecondTeam() {
-    return secondTeam;
-}
-public void setSecondTeam(Team secondTeam) {
-    this.secondTeam = secondTeam;
-}
-public Date getMatchDate() {
-    return matchDate;
-}
-public void setMatchDate(Date matchDate) {
-    this.matchDate = matchDate;
-}
-public String getVenue() {
-    return venue;
-}
-public void setVenue(String venue) {
-    this.venue = venue;
-}
-public String getResult() {
-    return result;
-}
-public void setResult(String result) {
-    this.result = result;
-}
-public String getStatus() {
-    return status;
-}
-public void setStatus(String status) {
-    this.status = status;
-}
-public Team getWinnerTeam() {
-    return winnerTeam;
-}
-public void setWinnerTeam(Team winnerTeam) {
-    this.winnerTeam = winnerTeam;
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int matchId;
+    private int firstTeamId;
+    private int secondTeamId;
+    private Date matchDate;
+    private String venue;
+    private String result;
+    private String status;
+    private int winnerTeamId;
+    public Match() {
+    }
+    public Match(int matchId, int firstTeamId, int secondTeamId, Date matchDate, String venue, String result,
+            String status, int winnerTeamId) {
+        this.matchId = matchId;
+        this.firstTeamId = firstTeamId;
+        this.secondTeamId = secondTeamId;
+        this.matchDate = matchDate;
+        this.venue = venue;
+        this.result = result;
+        this.status = status;
+        this.winnerTeamId = winnerTeamId;
+    }
+    public int getMatchId() {
+        return matchId;
+    }
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+    public int getFirstTeamId() {
+        return firstTeamId;
+    }
+    public void setFirstTeamId(int firstTeamId) {
+        this.firstTeamId = firstTeamId;
+    }
+    public int getSecondTeamId() {
+        return secondTeamId;
+    }
+    public void setSecondTeamId(int secondTeamId) {
+        this.secondTeamId = secondTeamId;
+    }
+    public Date getMatchDate() {
+        return matchDate;
+    }
+    public void setMatchDate(Date matchDate) {
+        this.matchDate = matchDate;
+    }
+    public String getVenue() {
+        return venue;
+    }
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+    public String getResult() {
+        return result;
+    }
+    public void setResult(String result) {
+        this.result = result;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public int getWinnerTeamId() {
+        return winnerTeamId;
+    }
+    public void setWinnerTeamId(int winnerTeamId) {
+        this.winnerTeamId = winnerTeamId;
+    }
+    @Override
+    public String toString() {
+        return "Match [matchId=" + matchId + ", firstTeamId=" + firstTeamId + ", secondTeamId=" + secondTeamId
+                + ", matchDate=" + matchDate + ", venue=" + venue + ", result=" + result + ", status=" + status
+                + ", winnerTeamId=" + winnerTeamId + "]";
+    }
 
 
+    
+    
 }
