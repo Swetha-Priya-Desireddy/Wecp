74397401e84a2cd7bcc281d82b3553c92777ea94
Swetha Priya <140045041+Swetha-Priya-Desireddy@users.noreply.github.com>
2025-09-04 09:41:35 +0530

    Day 7 Task

--- backend/src/main/java/com/wecp/progressive/repository/CricketerRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/CricketerRepository.java
@@ -2,21 +2,26 @@ package com.wecp.progressive.repository;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wecp.progressive.entity.Cricketer;
+
 @Repository
-public interface CricketerRepository extends JpaRepository<Cricketer,Integer>{
+public interface CricketerRepository extends JpaRepository<Cricketer, Integer> {
+
     Cricketer findByCricketerId(int cricketerId);
-    List<Cricketer>findByTeam_TeamId(int teamId);
+
+    List<Cricketer> findByTeam_TeamId(int teamId);
+
+    long countByTeam_TeamId(int teamId);
+
     @Modifying
     @Transactional
-    @Query("delete from Cricketer c where c.team.teamId = :teamId")
-    void deleteByTeamId(@Param("teamId")int teamId);
+    @Query("DELETE FROM Cricketer c WHERE c.team.teamId = :teamId")
+    void deleteByTeamId(@Param("teamId") int teamId);
 }
