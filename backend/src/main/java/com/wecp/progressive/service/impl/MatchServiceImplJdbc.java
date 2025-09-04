61f598c02ed09a0788b595e99ed24302a8ca85e2
Swetha Priya <140045041+Swetha-Priya-Desireddy@users.noreply.github.com>
2025-09-04 09:48:56 +0530

    Day 7 Task

--- backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJdbc.java
@@ -1,47 +1,45 @@
+
 package com.wecp.progressive.service.impl;
+
 import java.sql.SQLException;
 import java.util.List;
+
 import com.wecp.progressive.dao.MatchDAO;
 import com.wecp.progressive.entity.Match;
 import com.wecp.progressive.service.MatchService;
 
 public class MatchServiceImplJdbc implements MatchService {
+
     private MatchDAO matchDAO;
+
     public MatchServiceImplJdbc(MatchDAO matchDAO) {
         this.matchDAO = matchDAO;
     }
 
     @Override
-    public List<Match>getAllMatches()throws SQLException
-    {
+    public List<Match> getAllMatches() throws SQLException{
         return matchDAO.getAllMatches();
-
     }
-    @Override
 
-    public Match getMatchById(int matchId)throws SQLException
-    {
+    @Override
+    public Match getMatchById(int matchId) throws SQLException {
         return matchDAO.getMatchById(matchId);
     }
-    @Override
 
-    public Integer addMatch(Match match)throws SQLException
-    {
+    @Override
+    public Integer addMatch(Match match) throws SQLException {
         return matchDAO.addMatch(match);
     }
-    @Override
 
-    public void updateMatch(Match match)throws SQLException
-    {
+    @Override
+    public void updateMatch(Match match) throws SQLException{
         matchDAO.updateMatch(match);
     }
+
     @Override
-
-    public void deleteMatch(int matchId)throws SQLException
-    {
+    public void deleteMatch(int matchId) throws SQLException{
         matchDAO.deleteMatch(matchId);
-
+        
     }
-    
 
 }
